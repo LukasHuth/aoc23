14b38236580e4db1a3fb0828c7085afc0b997475
Lukas Huth <60547284+LukasHuth@users.noreply.github.com>
2023-12-07 19:19:39 +0000

    Fixed Part 1 and finished Part 2

--- src/days/day7.rs
+++ src/days/day7.rs
@@ -20,6 +20,114 @@ enum HandVariants {
     OnePair,
     HighCard,
 }
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct Card {
+    cards: [char; 5],
+    variant: HandVariants,
+    bid: usize,
+}
+fn part1(file_content: &String) -> usize {
+    let cards: Vec<Card> = utils::transform_lines(file_content.clone(), &Part1Parse);
+    let mut card_values: Vec<(usize, Card)> = cards.iter().map(|card| {
+        let sorted_chars = card.cards.map(|c|get_card_value_part1(c));
+        // sorted_chars.sort();
+        // println!("{:?}", sorted_chars);
+        let mut value = card.variant.get_value() << 20;
+        // println!("{:#b}", value);
+        value += sorted_chars[0] << 16;
+        // println!("{:#32x}", value);
+        ( value + (sorted_chars[1] << 12) + (sorted_chars[2] << 8) + (sorted_chars[3] << 4) + sorted_chars[4], *card)
+    }).collect();
+    card_values.sort_by(|a, b|a.0.cmp(&b.0));
+    let result: usize = card_values.iter().enumerate().map(|(i, (_, card))| {
+        (i+1) * card.bid
+    }).sum();
+    println!("Part1: {result}");
+    result
+}
+fn part2(file_content: &String) -> usize {
+    let cards: Vec<Card> = utils::transform_lines(file_content.clone(), &Part2Parse);
+    let mut card_values: Vec<(usize, Card)> = cards.iter().map(|card| {
+        let sorted_chars = card.cards.map(|c|get_card_value_part2(c));
+        // sorted_chars.sort();
+        // println!("{:?}", sorted_chars);
+        let mut value = card.variant.get_value() << 20;
+        // println!("{:#b}", value);
+        value += sorted_chars[0] << 16;
+        // println!("{:#32x}", value);
+        ( value + (sorted_chars[1] << 12) + (sorted_chars[2] << 8) + (sorted_chars[3] << 4) + sorted_chars[4], *card)
+    }).collect();
+    card_values.sort_by(|a, b|a.0.cmp(&b.0));
+    let result: usize = card_values.iter().enumerate().map(|(i, (_, card))| {
+        (i+1) * card.bid
+    }).sum();
+    println!("Part2: {result}");
+    result
+}
+struct Part1Parse;
+impl utils::TransformFunction<Card> for Part1Parse {
+    fn transform(&self, line: &str) -> Card {
+        let splited: Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
+        let cards_str = splited[0].clone();
+        let cards = cards_str.chars().collect::<Vec<_>>().try_into().unwrap();
+        let bid: usize = splited[1].parse().unwrap();
+        Card { cards , variant: HandVariants::get_variant(cards_str.as_str()), bid }
+    }
+}
+struct Part2Parse;
+impl utils::TransformFunction<Card> for Part2Parse {
+    fn transform(&self, line: &str) -> Card {
+        let mut best_variant: Option<HandVariants> = None;
+        for letter in ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"] {
+            let splited: Vec<String> = line.replace("J", letter).split(" ").map(|s| s.to_string()).collect();
+            let hv = HandVariants::get_variant(&splited[0].clone());
+            if let Some(bv) = best_variant{
+                if hv.get_value() > bv.get_value() { best_variant = Some(hv); }
+            } else { best_variant = Some(hv); }
+        }
+        let splited: Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
+        let cards_str = splited[0].clone();
+        let cards = cards_str.chars().collect::<Vec<_>>().try_into().unwrap();
+        let bid: usize = splited[1].parse().unwrap();
+        Card { cards , variant: best_variant.unwrap(), bid }
+    }
+}
+fn get_card_value_part1(c: char) -> usize {
+    match c {
+        '2' => 1,
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        '8' => 7,
+        '9' => 8,
+        'T' => 9,
+        'J' => 10,
+        'Q' => 11,
+        'K' => 12,
+        'A' => 13,
+        _ => panic!("Why? ({})", c)
+    }
+}
+fn get_card_value_part2(c: char) -> usize {
+    match c {
+        'J' => 1,
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'T' => 10,
+        'Q' => 11,
+        'K' => 12,
+        'A' => 13,
+        _ => panic!("Why? ({})", c)
+    }
+}
 impl HandVariants {
     fn get_value(&self) -> usize {
         match self {
@@ -61,64 +169,5 @@ impl HandVariants {
             4 => Self::OnePair,
             _ => panic!("{:?}", set),
         }
-    }
-}
-#[derive(Clone, Copy, PartialEq, Debug)]
-struct Card {
-    cards: [char; 5],
-    variant: HandVariants,
-    bid: usize,
-}
-fn part1(file_content: &String) -> usize {
-    let cards: Vec<Card> = utils::transform_lines(file_content.clone(), &Part1Parse);
-    let mut card_values: Vec<(usize, Card)> = cards.iter().map(|card| {
-        let mut sorted_chars = card.cards.map(|c|get_card_value(c));
-        sorted_chars.sort();
-        // println!("{:?}", sorted_chars);
-        let mut value = card.variant.get_value() << 20;
-        // println!("{:#b}", value);
-        value += sorted_chars[4] << 16;
-        // println!("{:#32x}", value);
-        ( value + (sorted_chars[3] << 12) + (sorted_chars[2] << 8) + (sorted_chars[1] << 4) + sorted_chars[0], *card)
-    }).collect();
-    card_values.sort_by(|a, b|a.0.cmp(&b.0));
-    for cv in card_values.iter().skip(900) {
-        println!("{:?}", cv.1);
-    }
-    let result: usize = card_values.iter().enumerate().map(|(i, (_, card))| {
-        (i+1) * card.bid
-    }).sum();
-    println!("Part1: {result}");
-    result
-}
-fn get_card_value(c: char) -> usize {
-    match c {
-        '2' => 1,
-        '3' => 2,
-        '4' => 3,
-        '5' => 4,
-        '6' => 5,
-        '7' => 6,
-        '8' => 7,
-        '9' => 8,
-        'T' => 9,
-        'J' => 10,
-        'Q' => 11,
-        'K' => 12,
-        'A' => 13,
-        _ => panic!("Why? ({})", c)
-    }
-}
-fn part2(file_content: &String) -> usize {
-    0
-}
-struct Part1Parse;
-impl utils::TransformFunction<Card> for Part1Parse {
-    fn transform(&self, line: &str) -> Card {
-        let splited: Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
-        let cards_str = splited[0].clone();
-        let cards = cards_str.chars().collect::<Vec<_>>().try_into().unwrap();
-        let bid: usize = splited[1].parse().unwrap();
-        Card { cards , variant: HandVariants::get_variant(cards_str.as_str()), bid }
     }
 }
